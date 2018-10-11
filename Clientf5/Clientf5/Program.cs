a9f5aa531abd86c3ea4f6a50732fe35cf8138c68
cutkit49 <tungld702@gmail.com>
2018-10-11 13:59:02 +0700

    this is commit

---
+++ Clientf5/Clientf5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientf5
+{ 
+    class Program
+    {
+    private const int FUFFER_SIZE = 1024;
+    private const int PORT_NUMBER = 9999;
+       // static UserEncoding encoding = new UserEncoding();
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                //1- Connect
+                client.Connect("127.0.0.1", PORT_NUMBER);
+                Stream stream = client.GetStream();
+                Console.WriteLine("Connected to Serverf5");
+                Console.WriteLine("Enter your name:");
+                string str = Console.ReadLine();
+                //2- Send
+
+                byte[] data = Encoding.Default.GetBytes(str);
+                stream.Write(data, 0, data.Length);
+                //3- Receive
+                data = new byte[FUFFER_SIZE];
+                stream.Read(data, 0, FUFFER_SIZE);
+                Console.WriteLine(Encoding.Default.GetString(data));
+                //4- Close
+                stream.Close();
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Error "+ e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Serverf5/Serverf5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Serverf5
+{
+    class Program
+    {
+        private const int BUFFER_SIZE = 1024;
+        private const int PORT_NUMBER = 9999;
+        ///static UserEncoding encoding = new UserEncoding();
+        static void Main(string[] args)
+        {
+            try
+            {
+            
+                while (true)
+                {
+                    IPAddress address = IPAddress.Parse("127.0.0.1");
+                    TcpListener listener = new TcpListener(address, PORT_NUMBER);
+                    //1- Listen
+                    listener.Start();
+                    Console.WriteLine("Server start on " + listener.LocalEndpoint);
+                    Socket socket = listener.AcceptSocket();
+                    Console.WriteLine("connection received from" + socket.RemoteEndPoint);
+                    //2- Received
+                    byte[] data = new byte[BUFFER_SIZE];
+                    socket.Receive(data);
+                    string str = Encoding.Default.GetString(data);
+                    //3- Send
+                    socket.Send(Encoding.Default.GetBytes("Got Heap Server Receive " + "Server Xin Chao Ban " + str));
+                    //4- Close
+                    socket.Close();
+                    listener.Stop();
+                }
+              
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.Message);
+                
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
